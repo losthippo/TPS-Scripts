8a221867154ef54e19985be977dacf0da75d569d
TPS - Lisa Hendry <lisa.hendry@turningpointscotland.com>
2020-03-19 17:52:53 +0000

    Initial commit

---
+++ update-data.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\Users\lisa.hendry\Desktop\Github\TPS-Dash-PEAR\venv\Scripts & activate & cd /d    C:\Users\lisa.hendry\Desktop\Github\TPS-Dash-PEAR\ & python fetch.py & python push.py"
